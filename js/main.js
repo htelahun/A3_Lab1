1b6875bc2ae994f973eea3550f8dcfa6534f1585
Telahun <h_telahun@fanshawec.ca>
2017-12-08 11:01:37 -0500

    video files

--- js/main.js
+++ js/main.js
@@ -2,6 +2,48 @@
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
 
 var carInfo = document.querySelectorAll('.data-ref');
+const httpRequest = new XMLHttpRequest();
+
+// function changeCars2(){
+//   const url =  './includes/functions.php?carModel=' + this.id;
+//   fetch (url)
+//   .then((resp)=> resp.json())
+//   .then((data)=> {processResult(data);})
+//   .catch(function (error){
+//     console.log(error);
+//   });
+  /*// set up AJAX call => handle errors first
+  if (!httpRequest){
+    alert('giving up not working');
+    return false;
+  }
+  httpRequest.onreadystatechange = processRequest;
+  httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
+  httpRequest.send();
+}
+
+function processRequest() {
+   let reqIndicator = document.querySelector('.request-state');
+   reqIndicator.textContent = httpRequest.readyState;
+   debugger;
+
+   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+     if (httpRequest.status === 200) { // 200 means everything is awesome
+       //debugger;
+       let data = JSON.parse(httpRequest.responseText); //turn data into js object
+
+
+   processResult(data);//send data to the function
+     } else {
+       alert('There was a problem with the request.');
+     }
+   }
+ }
+
+carInfo.forEach(function(element,index){
+  element.addEventListener('click', changeCars2, false);
+})*/
+
 var thumbInfo= document.querySelector('.thumbInfo');
 var car1 = document.querySelector('#F55');
 var car2 = document.querySelector('#F56');
@@ -13,22 +55,26 @@ var appliedClass;
 
 
   //link the images to the descriptions in the other folder with an index
-function changeCars(){
+function changeCars(data){
+  let objectIndex= carData[this.id];
+  const {model, price, details} = data; //deconstruction assignment
+  /*let modelName = document.querySelector('.modelName').textContent= model;
+  let priceInfo = document.querySelector('.priceInfo').innerHTML = price;
+  let modelDetails = document.querySelector('.modelDetails').textContent = details;*/
   //console.log("working");
-let objectIndex= carData[this.id];
-
 //opacity
 
   thumbInfo.classList.remove('nonActive');
 
   thumbInfo.classList.add('nonActive');
-
 //define the variables by their value in the index
 modelName.firstChild.nodeValue= objectIndex.model;
 priceInfo.firstChild.nodeValue= objectIndex.price;
 modelDetails.firstChild.nodeValue= objectIndex.details;
 
 appliedClass = this.id;
+//backticks are new ES6 template strings - look it up
+document.querySelector(`#${data.model}`).classList.remove('nonActive');
 }
 
 
